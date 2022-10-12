e2959a2827ca044f7e1457228981dc2e87388077
ruslaan.gilazefwgm <ruslaan.gilazefwgm@gmail.com>
2022-10-12 11:56:34 +0300

    Brifing Task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class Main {
+    public static int[] calculateTask(int[] m) {
+        for (int i = 0; i < m.length; i++) {
+            if (m[i] % 2 == 0) {
+                m[i] += 1;
+            }
+        }
+        int[] m2 = Arrays.copyOfRange(m,2, 6);
+        return m2;
+    }
+    public static void main(String[] args) {
+        int[] massive = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println(Arrays.toString(calculateTask(massive)));
     }
 }
